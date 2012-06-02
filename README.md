087049aa096e1d275924eccb895cc46e55fd0915
Dmytro Yashkir <dima.yashkir@gmail.com>
2012-06-02 16:02:57 -0400

    sqlite added

--- README.md
+++ README.md
@@ -8,20 +8,68 @@ All addresses
 
     /addresses
      
-    [
+[
     {
         "id": 1,
-        "lat": "43.65",
-        "lon": "-79.38",
-        "address": "123 some sort of street"
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
     },
     {
         "id": 2,
-        "lat": "43.64",
-        "lon": "-79.39",
-        "address": "123 some sort of street"
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 3,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 4,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 5,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 6,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 7,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 8,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 9,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
+    },
+    {
+        "id": 10,
+        "lat": 43.64,
+        "lon": -79.39,
+        "line": "Toronto something"
     }
-    ]
+]
 
 All incidents for specific address
 ==================================
--- app.js
+++ app.js
@@ -4,9 +4,38 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
-
+  , routes = require('./routes')
+  , sqlite3 = require('sqlite3').verbose()
+  , testDB = new sqlite3.Database('tester.db')
+  , db = new sqlite3.Database('real_stuff.db');
 var app = module.exports = express.createServer();
+
+//////
+//
+//
+//
+testDB.serialize(function() {
+  
+  testDB.run("CREATE TABLE ADDRESS (lat NUMERIC, lon NUMERIC, line varchar(255))");
+
+
+
+  var stmt = testDB.prepare("INSERT INTO ADDRESS VALUES (?, ?, ?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run([43.64, -79.39, 'Toronto something']);
+  }
+  stmt.finalize();
+
+  testDB.each("SELECT rowid AS id, lat, lon, line FROM ADDRESS", function(err, row) {
+    if (err){
+      console.log(err);
+    }else{
+      console.log(row.id + ": " + row.line + " lat:" + row.lat + " lon " + row.lon);
+    }
+  });
+});
+
+db.close();
 
 // Configuration
 
@@ -32,9 +61,18 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 app.get('/addresses', function(req, res){
-  var mockup = [  {id:1, lat:'43.65', lon:'-79.38', address:'123 some sort of street'},
-                  {id:2, lat:'43.64', lon:'-79.39', address:'123 some sort of street'}];
-  res.send(mockup);
+  var mockup = [];
+  testDB.each("SELECT rowid AS id, lat, lon, line FROM ADDRESS", function(err, row) {
+    if (err){
+      console.log(err);
+    }else{
+      console.log(row.id + ": " + row.address_line + " lat:" + row.lat + " lon " + row.lon);
+    }
+    mockup.push(row);
+  },
+  function(){
+    res.send(mockup);
+  });
 });
 
 app.listen(3000, function(){
